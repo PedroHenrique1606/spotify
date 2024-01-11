d3937b82acf96ba5f001c8c594f971bcb64764f5
PedroHenrique1606 <pedromelosilva11911@gmail.com>
2024-01-11 14:27:29 -0300

    ui finalized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import {Home as HomeIcon , Search, Library, ChevronLeft, ChevronRight, Play} from 'lucide-react'
+import {Home as HomeIcon , Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat, Mic2, LayoutList, Laptop2, Volume, Maximize, Maximize2} from 'lucide-react'
 
 export default function Home() {
   return(
-    <div className="h-screen flex flex-col">
+    <div className="h-screen flex flex-1 flex-col overflow-y-auto-auto custom-scrollbar">
       <div className="flex flex-1">
         <aside className="w-72 bg-zinc-950 p-6">
           <div className='flex items-center gap-2'>
@@ -97,13 +97,73 @@ export default function Home() {
           </div>
 
           <h2 className='font-semibold text-2xl mt-10'>Made For You</h2>
-          <div className='grid grid-cols-5 gap-4 mt-4'>
-
+          <div className='grid grid-cols-8 gap-4 mt-4'>
+            <a href='' className='bg-white/5 p-3 flex flex-col gap-2 rounded-md hover:bg-white/10'>
+            <img className='w-full ' src="/images/cd-cover-mj.jpg" width={70} height={70} alt="" />
+            <strong className='font-semibold'>Daily Mix 1</strong>
+            <span className='text-sm text-zinc-500'>Michael Jackson</span>
+            </a>
+            <a href='' className='bg-white/5 p-3 flex flex-col gap-2 rounded-md hover:bg-white/10'>
+            <img className='w-full ' src="/images/cd-cover-mj.jpg" width={70} height={70} alt="" />
+            <strong className='font-semibold'>Daily Mix 2</strong>
+            <span className='text-sm text-zinc-500'>Michael Jackson</span>
+            </a>
+            <a href='' className='bg-white/5 p-3 flex flex-col gap-2 rounded-md hover:bg-white/10'>
+            <img className='w-full ' src="/images/cd-cover-mj.jpg" width={70} height={70} alt="" />
+            <strong className='font-semibold'>Daily Mix 3</strong>
+            <span className='text-sm text-zinc-500'>Michael Jackson</span>
+            </a>
+            <a href='' className='bg-white/5 p-3 flex flex-col gap-2 rounded-md hover:bg-white/10'>
+            <img className='w-full ' src="/images/cd-cover-mj.jpg" width={70} height={70} alt="" />
+            <strong className='font-semibold'>Daily Mix 4</strong>
+            <span className='text-sm text-zinc-500'>Michael Jackson</span>
+            </a>
+            <a href='' className='bg-white/5 p-3 flex flex-col gap-2 rounded-md hover:bg-white/10'>
+            <img className='w-full ' src="/images/cd-cover-mj.jpg" width={70} height={70} alt="" />
+            <strong className='font-semibold'>Daily Mix 5</strong>
+            <span className='text-sm text-zinc-500'>Michael Jackson</span>
+            </a>
           </div>
         </main>
       </div>
-      <footer className="bg-zinc-800 border-t border-zinc-700 p-6 fixed bottom-0 w-full">
-        footer
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-6 fixed bottom-0 w-full flex items-center justify-between">
+        <div className='flex items-center gap-3'>
+        <img src="/images/cd-cover-paramore.jpg" width={56} height={56} alt="" />
+        <div className='flex flex-col'>
+          <strong className='font-normal'>The Only Exception</strong>
+          <span className='text-xs text-zinc-400'>Paramore</span>
+        </div>
+        </div>
+        <div className='flex flex-col items-center gap-2'>
+          <div className='flex items-center gap-6'>
+            <Shuffle size={20} className='text-zinc-200 '/>
+            <SkipBack size={20} className='text-zinc-200 '/>
+            <button className='w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black'>
+              <Play />
+            </button>
+            <SkipForward size={20} className='text-zinc-200 '/>
+            <Repeat size={20} className='text-zinc-200 '/>
+          </div>
+          <div className='flex items-center gap-2'>
+            <span className='text-sm text-zinc-400'>0:31</span>
+            <div className='h-1 rounded-full w-96 bg-zinc-600'>
+              <div className='bg-zinc-200 w-40 h-1 rounded-full'></div>
+            </div>
+            <span className='text-sm text-zinc-400'>2:14</span>
+          </div>
+        </div>
+        <div className='flex items-start gap-4'>
+          <Mic2 size={18}/>
+          <LayoutList size={18}/>
+          <Laptop2 size={18}/>
+          <div className='flex items-center gap-2'>
+            <Volume size={18} />
+            <div className='h-1 rounded-full w-24 bg-zinc-600'>
+            <div className='bg-zinc-200 w-10 h-1 rounded-full'></div>
+            </div>
+          </div>
+          <Maximize2 size={18} />
+        </div>
       </footer>
     </div>
   )
